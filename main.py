e7610dea3438155ff445a580f0695b614db9537e
Akash-M1 <akashm.cs20@bmsce.ac.in>
2021-07-02 18:23:56 +0530

    Changes

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 cap.set(10, 100)
-while True:
+swhile True:
     success, img = cap.read()
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    faces = face_cascade.detectMultiScale(gray, 1.4, 5)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
