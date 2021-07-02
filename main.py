977231cf969546799295a15c0f787444404bde24
Akash-M1 <akashm.cs20@bmsce.ac.in>
2021-07-02 18:09:04 +0530

    face Detection model

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('/Users/akashm/PycharmProjects/opencvPython/haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+cap.set(10, 100)
+while True:
+    success, img = cap.read()
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.putText(img, "Face Recognised", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+    cv2.imshow("video", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
